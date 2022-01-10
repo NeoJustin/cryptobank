ee87a12fa517f6809e0635b3c439f38256c120dd
Justin de Jong <justin.de.jong@rigd-loxia.nl>
2022-01-10 16:03:16 +0100

    Poging tot Lombok niet gelukt ;)

---
+++ src/main/java/koningjustin/cryptobank/domain/CryptoCurrencyLombok.java
@@ -0,0 +1,21 @@
+package koningjustin.cryptobank.domain;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.UUID;
+
+@JsonSerialize
+@JsonDeserialize(
+        builder = CryptoCurrencyLombok.CryptoCurrencyLombokBuilder.class,
+        as = CryptoCurrencyLombok.class)
+@Value
+@Builder
+public class CryptoCurrencyLombok {
+
+    UUID id;
+    Integer worth; // In dollars
+
+}
